397728da64de1b4db7c23888d614ab7031f0b75c
Khadidja CH <39216434+KhadidjaCroft@users.noreply.github.com>
2020-12-01 00:22:35 +0100

    Update try.php

--- try.php
+++ try.php
@@ -2,7 +2,19 @@
 $server='localhost';
 $user='root';
 $password='';
-$conn= new mysqli(server,user,password)
+$conn= new mysqli($server,$user,$password);
 if ($conn->connect_error) {die('Connection failed!' . $conn->connect_error);}
 echo 'Successfully connected.';
+$sql='select name,email from users';
+$result=$conn->query($sql);
+if ($result->num_rows > 0)
+ {
+ 	while($row=$result->fetch_assoc()){
+ 		echo $row['name'] . $row['email'] . '<br>';
+ 	}
+ }
+ else { echo 'No entries found.';}
+ $conn->close();
 ?>
+
+//$last_id=$conn->insert_id; for getting last inserted row
